32eee0896ec6e26826aca03533a1bd44d837036b
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-09-13 14:25:32 +0300

    Add HomeView widget and update main app structure

---
+++ lib/views/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(leading: Icon(Icons.menu,color: Colors.white,)
+      ,
+      backgroundColor: Colors.black,
+      ),
+      
+    );
+  }
+}
